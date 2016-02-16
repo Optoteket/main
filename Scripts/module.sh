563c37fc3a12029754b01fbe26a58d7956986566
Emelie Karlsson <emeliemariatherese.k@gmail.com>
2016-02-16 15:48:16 +0100

    Small changes

--- Scripts/module.sh
+++ Scripts/module.sh
@@ -7,5 +7,5 @@ echo "Adding CPLEX module"
 # Load module definitions
 . /sw/modules/init/bash.init
 module add cplex/12.5-fullampl
-ampl > /edu/emeka813/MasterThesis/AMPLmodel/librarystaff.run
+ampl < /edu/emeka813/MasterThesis/AMPLmodel/librarystaff.run
 echo "Module added"
