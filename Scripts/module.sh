5656fe97e975eaea39db30b86b7d27f1580100a0
Claes Arvidson <claar324@student.liu.se>
2016-02-16 17:04:22 +0100

    Major changes in the workspace! (problem with 1..x)

--- Scripts/module.sh
+++ Scripts/module.sh
@@ -7,5 +7,8 @@ echo "Adding CPLEX module"
 # Load module definitions
 . /sw/modules/init/bash.init
 module add cplex/12.5-fullampl
-ampl < /edu/emeka813/MasterThesis/AMPLmodel/librarystaff.run
 echo "Module added"
+clear
+clear
+ampl < ../AMPLmodel/librarystaff.run
+
