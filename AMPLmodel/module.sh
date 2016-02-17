601182324e6e27f610d86922c96bfdd7af72b4ab
Emelie Karlsson <emeliemariatherese.k@gmail.com>
2016-02-17 09:54:22 +0100

    Fixed mergeproblem?

--- AMPLmodel/module.sh
+++ AMPLmodel/module.sh
@@ -7,5 +7,5 @@ echo "Adding CPLEX module"
 # Load module definitions
 . /sw/modules/init/bash.init
 module add cplex/12.5-fullampl
-ampl < ../AMPLmodel/librarystaff.run
+ampl < librarystaff.run
 echo "Module added"
