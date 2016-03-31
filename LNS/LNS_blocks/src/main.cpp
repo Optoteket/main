456f67820bc6146cf1f7ece9e67a7090ce5328e1
Claes Arvidson <claar324@student.liu.se>
2016-03-31 16:09:36 +0200

    End of day

--- LNS/LNS_blocks/src/main.cpp
+++ LNS/LNS_blocks/src/main.cpp
@@ -8,19 +8,38 @@ using namespace std;
 
 int main() {
 	ifstream inFile;
-	inFile.open("./src/numbers.txt");
+	inFile.open("./src/data/librarystaff5W.txt");
 	//Checking for open Error
 	if (inFile.fail()) {
 		cerr << "Error opening the file!" << endl;
 		exit(1);
 	}
+	int count = 0;
+	string line;
+	if (inFile.good())
+	{
+		while (!inFile.good())
+		{
+			getline(inFile, line);
+			if (line == "lib")
+			{
+				count++;
+			}
+		}
+		inFile.close();
+		cout << count << endl;
+	}
+	else cout << "End of file \n";
+	
+	return 0;
+	
 // 	inFile << "Hello world\n";
 // 	inFile.close();
 
-	int x,y;
-	inFile >> x >> y;
-	cout << "Num1: " << x << endl;
-	return 0;
+// 	int x,y;
+// 	inFile >> x >> y;
+// 	cout << "Num1: " << x << endl;
+// 	return 0;
 // 	string name;
 	
 // 	cout << "Hello world" << endl;
