8cd5c06b1ab3e09d8087125f709dcde4592fc29e
Claes Arvidson <claar324@student.liu.se>
2016-04-06 12:09:16 +0200

    Before lunch

--- LNS/LNS_blocks/src/Workers.cpp
+++ LNS/LNS_blocks/src/Workers.cpp
@@ -1,4 +1,11 @@
 #include "Workers.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <stdlib.h> //Might be needed for exit(1) on some compilers!
+#include <sstream>
+#include <iomanip>
 
 Worker::Worker() {
 	newID = 0;
@@ -95,36 +102,44 @@ void Worker::setPL(string pl) {
 	newPL = pl;
 }
 
-// void Worker::setAvail(string file_to_open) {
-// 	ifstream inFile(file_to_open);
+// void Worker::setAvail() {
+// 	ifstream inFile(avail_file);
+// 	
 // 	//Checking for open Error
 // 	if (inFile.fail()) {
 // 		cerr << "Error opening the file!" << endl;
 // 		exit(1);
 // 	}
-// 
+// 	
 // 
 // 	vector<int> vec; 
 // 	//int a, b, c;
 // 	string readline;
 // 	string integer_string;
 // 	string avail_string;
-// 	int count = 0;
+// 	int myID = getID();
 // 	while( inFile.good() )
 // 	{
 // 		getline( inFile, readline);
 // 		//cout<< "\n" << readline <<"\n";
-// 		size_t found = readline.find("=");
-// 		size_t pos = readline.find (" ");
+// 		size_t found = readline.find("="); //Find the character with an '='
+// 		size_t pos = readline.find (",");
 // 		if(found != string::npos)
 // 		{
 // 			//while (pos != string::npos){
-// 			integer_string = readline.substr(0,found-1); //getting the string "1 1 1"
-// 			avail_string = readline.substr(found+1);
+// 			integer_string = readline.substr(0,found-1); //getting the string "1,1,1"
+// 			avail_string = readline.substr(found+1); //getting the avail number as strings
 // 			cout << integer_string << endl;
-// 			//if '=' is found, then read the integers a b c = d
-// 			cout << found << endl;
-// 			cout << "an '=' found at: " << found << '\n';
+// 			cout << avail_string << endl;
+// 		}
+// 		
+// 		//Write the avail for [w,d,s] to worker_avail[w][d][s]
+// 		for (int w=0; w<NUM_WEEKS; w++){
+// 			for(int d=0; d<NUM_DAYS; d++){
+// 				for(int s=0; s<NUM_SHIFTS; s++){
+// 					worker_avail[w][d][s] = atoi(avail_string);
+// 				}
+// 			}
 // 		}
 // // 		cout<< "\n" << readline <<"\n";
 // 
--- LNS/LNS_blocks/src/main.cpp
+++ LNS/LNS_blocks/src/main.cpp
@@ -8,40 +8,88 @@
 #include "Workers.h"
 using namespace std;
 
-int main() {
-	ifstream inFile("./src/data/librarystaff5W.txt");
+//Assigns avail for all workers
+void setAvail() {
+	ifstream inFile("./src/data/workers5W.txt");
+	
 	//Checking for open Error
 	if (inFile.fail()) {
 		cerr << "Error opening the file!" << endl;
 		exit(1);
 	}
+	
 
-
-	vector<int> vec; 
-	//int a, b, c;
+	vector<string> input_vector; 
 	string readline;
 	string integer_string;
 	string avail_string;
-	int count = 0;
+	//int wID = 0;
+	string wID;
+	int line_read_num = 0;
+	int workers_counted = 0;
+// 	int number_of_workers_in_total = 39;
+// 	int week, day, shift;
 	while( inFile.good() )
 	{
+		//Read until an "=" is found and assign all availability
 		getline( inFile, readline);
-		//cout<< "\n" << readline <<"\n";
-		size_t found = readline.find("=");
-		size_t pos = readline.find (" ");
-		if(found != string::npos)
+		line_read_num++; //the line read is number: line_read_num
+		input_vector.clear(); //input_vector contains the integers "week" "day" "shift" read from a line
+		if(line_read_num == 6+152*workers_counted)
 		{
-			//while (pos != string::npos){
-			integer_string = readline.substr(0,found-1); //getting the string "1 1 1"
-			avail_string = readline.substr(found+1);
-			cout << integer_string << endl;
-			//if '=' is found, then read the integers a b c = d
-			cout << found << endl;
-			cout << "an '=' found at: " << found << '\n';
+			//Assign the ID of the worker that is being read
+			//wID = stoi(readline,nullptr);
+			wID = readline;
+			cout << wID << endl;
 		}
-	
+		
+		
+		size_t found = readline.find("="); //Find the character with an "="
+		size_t pos = readline.find (",");
+		if(found != string::npos) //separate a readline if containing an "="
+		{
+			
+			integer_string = readline.substr(0,found-1); //getting the string "1,1,1"
+			avail_string = readline.substr(found+2); //getting the avail number as strings
+			//cout << "the integer string is:" << integer_string << endl;
+			//separate all three integers in integer_string into input_vector
+			while(pos != string::npos)
+			{
+				input_vector.push_back(integer_string.substr(0,pos)); //adds everything that appears before the first ","
+				//cout << "The integer added to the vector is:" << integer_string.substr(0,pos) << endl;
+				integer_string = integer_string.substr(pos+1); //Everything after the "," is in the new integer_string
+				//cout << integer_string << endl;
+				pos = integer_string.find(","); //Find the position of the next "," in the new readline
+			}
+			//Add the last integer (as string) to input_vector
+			input_vector.push_back(integer_string.substr(0,pos));
+			//cout << "The integer added to the vector is:" << integer_string.substr(0,pos) << endl;
+			
+// 				shift = stoi(input_vector.back(),nullptr);
+// 				input_vector.pop_back();
+// 				day = stoi(input_vector.back(),nullptr);
+// 				input_vector.pop_back();
+// 				week = stoi(input_vector.back(),nullptr);
+// 				input_vector.pop_back();
+			
+			//Assign the availability to the workers 
+			//worker_list[wID].worker_avail[week][day][shift] = stoi(avail_string,nullptr);
+			
+		}
+		size_t found_new_worker = readline.find ("avail:");
+		if(found_new_worker != string::npos) //A line is read containing "avail:"
+		{
+			workers_counted++;
+		}
+		
+		
 	}
-	cout << count << endl;
+}
+
+int main() {
+	//create a list of worker objects. worker_list[1], worker_list[2] etc.
+	//Worker worker;
+	setAvail();
 	return 0;
 }
 
