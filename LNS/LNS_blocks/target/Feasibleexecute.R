90973cf514602eb9f4923218940caa8565565266
Claes Arvidson <claar324@student.liu.se>
2016-06-08 13:02:21 +0200

    Before checking out feasible

---
+++ LNS/LNS_blocks/target/Feasibleexecute.R
@@ -0,0 +1,28 @@
+# Read values from text file. objfcn, feasible, standins and maxmin
+plot_data <- read.table("./target/Saved results/Plots for Report/1phase1iterRerun/feasible.txt", header=T, sep="\t") #Change this path!
+
+#Compute max y-values
+max_y_feasible <- max(plot_data$feasible)
+
+
+
+#Define colors to be used
+plot_colors <- c("blue")
+
+# Start PNG device driver to save output to figure.png
+png(filename="./target/Saved results/Plots for Report/1phase1iterRerun/Rfeasible.png", height=1300, width=2600, bg="white")
+#jpeg('rplot.jpg')
+
+#plot each of the columns
+plot(plot_data$feasible, col = plot_colors[1])
+#Make x-axis
+#axis(1, at=1:5, lab=c("Mon", "Tue", "Wed", "Thu", "Fri"))
+#Make y-axis
+#axis(2, las=1, at=4*0:max_y_objfcn)
+
+
+# Turn off device driver (to flush output to png)
+dev.off()
+
+
+
